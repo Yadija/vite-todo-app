60651586355c0c30813dc5742a37f7eaaa1e1a37
Yadija <ayuliadi01@gmail.com>
2023-09-02 14:38:29 +0800

    install react icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
+import { IoTrashOutline } from 'react-icons/io5';
+import { MdCheckBox, MdCheckBoxOutlineBlank } from 'react-icons/md';
+import { TbMoon, TbSunHigh } from 'react-icons/tb';
+
 function App() {
   return (
     <div className='dark'>
       <div className='h-screen w-full bg-softPeach text-funBLue dark:bg-balticSea dark:text-denimBLue'>
         <nav className='fixed left-0 right-0 top-0 flex justify-between px-5 py-3'>
-          <h1 className='font-bold'>Todo App</h1>
+          <h1 className='text-xl font-bold'>Todo App</h1>
           <div>
             <ul>
-              <li>Mode</li>
+              <li className='cursor-pointer text-2xl'>
+                <TbMoon />
+              </li>
             </ul>
           </div>
         </nav>
@@ -15,7 +21,7 @@ function App() {
           <form className='flex gap-4 pb-6'>
             <input
               type='text'
-              className='w-full px-4 py-2 dark:bg-softPeach dark:text-balticSea'
+              className='focus:ring-blueborder-blue-300 w-full px-4 py-2 focus:border-blue-300 focus:outline-none focus:ring-2 dark:bg-softPeach dark:text-balticSea'
               placeholder='Add...'
             />
             <button className='bg-funBLue px-4 py-2 font-bold text-softPeach dark:bg-denimBLue dark:text-balticSea'>
@@ -27,7 +33,7 @@ function App() {
           <div className='mb-6'>
             <input
               type='text'
-              className='w-full px-4 py-2 dark:bg-softPeach  dark:text-balticSea'
+              className='focus:ring-blueborder-blue-300 w-full px-4 py-2  focus:border-blue-300  focus:outline-none focus:ring-2 dark:bg-softPeach dark:text-balticSea'
               placeholder='Search...'
             />
           </div>
@@ -38,12 +44,14 @@ function App() {
             <ul className='flex flex-col gap-2'>
               <li>
                 <div className='flex justify-between'>
-                  <div className='flex w-full gap-4 bg-funBLue px-3 py-2 text-softPeach dark:bg-denimBLue dark:text-balticSea'>
-                    <input type='checkbox' />
+                  <div className=' flex w-full cursor-pointer gap-4 bg-funBLue px-3 py-2 text-softPeach hover:opacity-90 dark:bg-denimBLue dark:text-balticSea'>
+                    <div className='text-2xl'>
+                      <MdCheckBoxOutlineBlank />
+                    </div>
                     <p>Task 1</p>
                   </div>
-                  <div className='bg-white p-2 text-funBLue dark:bg-softPeach dark:text-balticSea'>
-                    delete
+                  <div className='cursor-pointer border border-blue-300 bg-white p-2 text-2xl text-funBLue hover:bg-red-500 hover:text-softPeach dark:bg-softPeach dark:text-balticSea dark:hover:bg-red-500 dark:hover:text-softPeach'>
+                    <IoTrashOutline />
                   </div>
                 </div>
               </li>
